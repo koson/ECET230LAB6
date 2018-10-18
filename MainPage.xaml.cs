ec1f3ad2765ffa1c646244a60cd11b1824467146
AlexGowans <alexgowans@hotmail.co.uk>
2018-10-17 19:35:36 -0700

    Uncommented step 2 C code

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -30,10 +30,11 @@ using Windows.UI.Xaml.Media;
 
 // Begin "Step 2: Use InkCanvas to support basic inking"
 ////using directives for inking functionality.
-//using Windows.UI.Input.Inking;
-//using Windows.UI.Input.Inking.Analysis;
-//using Windows.UI.Xaml.Shapes;
-//using Windows.Storage.Streams;
+///WE NEED THIS STUFF TO ACTUALLY DRAW 'N' TING
+using Windows.UI.Input.Inking;
+using Windows.UI.Input.Inking.Analysis;
+using Windows.UI.Xaml.Shapes;
+using Windows.Storage.Streams;
 // End "Step 2: Use InkCanvas to support basic inking"
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
